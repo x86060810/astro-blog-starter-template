53de18b89a26cfb52a2a5d177fb6476fcf638c48
x86060810 <284738715@qq.com>
2026-03-30 21:10:55 +0800

    更新文章

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
 	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "夏士博的网站 - 一人自治管理有限公司";
+export const SITE_TITLE = "夏士博的网站 - 一人自治管理有限公司 OPC";
 export const SITE_DESCRIPTION = "夏士博的个人网站，展示技术专长和职业历程";
