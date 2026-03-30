b699c70d8c951a43145dc461d78c114dd7480b9a
x86060810 <284738715@qq.com>
2026-03-30 21:17:20 +0800

    Fix: Add Cloudflare adapter and rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
+	adapter: cloudflare(),
 	integrations: [mdx(), sitemap()],
 });
